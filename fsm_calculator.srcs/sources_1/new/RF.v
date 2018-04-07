00f9f979fb5f0e1229078512130af5a1a9ff57a9
nickschiffer <nick@schiffer.us>
2018-04-07 16:07:32 -0700

    Added DP, MUX1, RF, ALU, and MUX2 modules

---
+++ fsm_calculator.srcs/sources_1/new/RF.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/07/2018 03:48:44 PM
+// Design Name: 
+// Module Name: RF
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RF(
+input clk, rea, reb, we,
+input [1:0] raa, rab, wa,
+input [2:0] din,
+output reg [2:0] douta, doutb
+);
+
+reg [2:0] RegFile [3:0];
+
+always @ (rea, reb, raa, rab)
+begin
+        if (rea)
+            douta = RegFile[raa];
+        else douta = 3'b000;
+        if (reb)
+            doutb = RegFile[rab];
+        else doutb = 3'b000;
+end
+
+always @ (posedge clk)
+begin
+        if(we)
+            RegFile[wa] <= din;
+        else
+            RegFile[wa] <= RegFile[wa];
+end
+endmodule
