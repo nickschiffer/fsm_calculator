f9f9f3ecf9a57a9f39ba87ff5e5c891596fe8145
nickschiffer <nick@schiffer.us>
2018-04-07 16:33:02 -0700

    Created registries and DUT in DP_tb.v

--- fsm_calculator.srcs/sim_1/new/DP_tb.v
+++ fsm_calculator.srcs/sim_1/new/DP_tb.v
@@ -22,5 +22,23 @@
 
 module DP_tb;
 
+//TB reg's
+reg [2:0] in1_tb, in2_tb, out_tb;
+reg [1:0] s1_tb, wa_tb, raa_tb, rab_tb, c_tb;
+reg we_tb, rea_tb, reb_tb, s2_tb, clk_tb;
 
+//DUT
+DP DUT(.in1(in1_tb),
+.in2(in2_tb),
+.s1(s1_tb),
+.wa(wa_tb),
+.raa(raa_tb),
+.rab(rab_tb),
+.c(c_tb),
+.we(we_tb),
+.rea(rea_tb),
+.reb(reb_tb),
+.s2(s2_tb),
+.clk(clk_tb),
+.out(out_tb));  
 endmodule
