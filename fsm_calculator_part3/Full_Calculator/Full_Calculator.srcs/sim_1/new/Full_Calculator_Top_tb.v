576c1db6225476402e6416c026cae9ddd6ad68a6
nickschiffer <nick@schiffer.us>
2018-05-06 18:05:16 -0700

    bugs with timings

--- fsm_calculator_part3/Full_Calculator/Full_Calculator.srcs/sim_1/new/Full_Calculator_Top_tb.v
+++ fsm_calculator_part3/Full_Calculator/Full_Calculator.srcs/sim_1/new/Full_Calculator_Top_tb.v
@@ -20,7 +20,71 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Full_Calculator_Top_tb(
+module Full_Calculator_Top_tb;
 
+reg [3:0] X_tb, Y_tb;
+reg go_tb, clk_tb, rst_tb;
+reg [2:0] F_tb;
+wire [3:0] Out_H_tb, Out_L_tb;
+wire done_tb;
+wire [3:0] cs;
+
+Full_Calculator_Top FCALC (
+    .X(X_tb), .Y(Y_tb),
+    .F(F_tb),
+    .clk(clk_tb), .Go(go_tb), .rst(rst_tb),
+    .Out_H(Out_H_tb), .Out_L(Out_L_tb),
+    .done(done_tb),
+    .cs(cs) 
     );
+    
+    
+task automatic tick;
+    begin
+        clk_tb = 1'b1;
+        #50;
+        clk_tb = 1'b0;
+        #50;
+    end
+endtask
+
+task automatic display;
+    begin
+        $display("Out_H_tb = %0d", Out_H_tb);
+        $display("Out_L_tb = %0d", Out_L_tb);
+        $display("Current State = %0d", cs);
+    end
+endtask
+
+integer i = 0;
+initial
+    begin
+        X_tb = 4'd3;
+        Y_tb = 4'd4;
+        clk_tb = 1'b0;
+        go_tb = 1'b0;
+        clk_tb = 1'b0;
+        rst_tb = 1'b0;
+        
+        
+        F_tb = 3'b000;
+        go_tb = 1'b1;
+        
+        tick;
+        
+//        while (done_tb == 0)
+//            begin
+//                tick;
+//                display;
+//            end
+        for (i = 0; i < 100; i = i + 1)
+            begin
+                tick;
+                display;
+            end
+        
+        
+    
+    $finish;
+    end
 endmodule
--- fsm_calculator_part3/Full_Calculator/Full_Calculator.srcs/sources_1/imports/new/Full_Calculator_CU.v
+++ fsm_calculator_part3/Full_Calculator/Full_Calculator.srcs/sources_1/imports/new/Full_Calculator_CU.v
@@ -33,7 +33,8 @@ module Full_Calculator_CU(
     output [1:0] Sel_L,
     output Calc_Mux_Sel, Mul_Mux_Sel,
     output En_Out_H, En_Out_L,
-    output RST_OUT_H, RST_OUT_L
+    output RST_OUT_H, RST_OUT_L,
+    output [3:0] cs
     );
     
     // Encode States
@@ -58,57 +59,67 @@ module Full_Calculator_CU(
 
     reg Calc_Mux_Sel_internal, Mul_Mux_Sel_internal;
     
+    reg calc;
+    
     // Next-State Logic (combinational) based on the state transition diagram
     always @ (CS, Go)
     begin
         case (CS)
-            S0: NS <= (Go) ? S1 : S0;
-            S1: NS <= S2;
+            S0: NS = (Go) ? S1 : S0;
+            S1: NS = S2;
             S2:
                 begin
                 if (F[2])
                     begin
-                        Calc_Mux_Sel_internal <= 1'b1;
-                        Mul_Mux_Sel_internal <= 1'b1;   
+                        Calc_Mux_Sel_internal = 1'b1;
+                        Mul_Mux_Sel_internal = 1'b1;   
                     end
                 else
                     begin
-                        Calc_Mux_Sel_internal <= 1'b0;
-                        Mul_Mux_Sel_internal <= 1'b0;
+                        Calc_Mux_Sel_internal = 1'b0;
+                        Mul_Mux_Sel_internal = 1'b0;
                     end
                 
                 if (F[2] && F[0])
-                    NS <= S0;
+                    NS = S0;
                 else
                     begin
                         if (F[1])
                             begin
                                 if (F[0])
-                                    NS <= S5;
+                                    NS = S5;
                                 else
-                                    NS <= S4;
+                                    NS = S4;
                             end
                         else
-                            NS <= S3;
+                            NS = S3;
                     end
                 end
-            S3: NS <= S6;
-            S4: NS <= S4p;
-            S5: NS <= S7;
-            S6: NS <= (Done_Calc) ? S9 : S6;
-            S7: NS <= (Done_Div) ? S9 : S7;
-            S8: NS <= S9;
-            S9: NS <= S0;
-            default: NS <= S0;
+            S3: NS = S6;
+            S4: NS = S4p;
+            S5: NS = S7;
+            S6: NS = (Done_Calc) ? S9 : S6;
+            S7: NS = (Done_Div) ? S9 : S7;
+            S8: NS = S9;
+            S9: NS = S0;
+            default: NS = S0;
         endcase
     end
     
     //State Register (sequential)
     always @ (posedge clk, posedge rst)
+        
         if (rst)
-            CS <= S0;
+            CS = S0;
+//        else if ((CS == S6) && (Done_Calc))
+//            begin
+//            //CS = S9;
+//            cw = 15'b__0_____0_____0_____0________0_______00________0_____10_______1_________1________0__________0_________0;
+//            calc = 1'b1;
+//            CS = S9;
+//            end
         else
-            CS <= NS;
+            CS = NS;
             
      //Output Logic (combinational) based on output table
        always @ (CS)
@@ -118,7 +129,7 @@ module Full_Calculator_CU(
                     begin
                     // cw <= {En_F, En_X, En_y, Go_Calc, Go_Div, Op_Calc, Sel_H, Sel_L, En_Out_H, En_Out_L, RST_OUT_H, RST_OUT_L, done}
                     cw <= 15'b__0_____0_____0_____0________0_______00________0_____00_______0_________0________1__________1________0;
-                
+                    calc <= 1'b0; 
                     end
                S1:
                     begin
@@ -169,7 +180,7 @@ module Full_Calculator_CU(
                     else
                     begin
                     // cw <= {En_F, En_X, En_y, Go_Calc, Go_Div, Op_Calc, Sel_H, Sel_L, En_Out_H, En_Out_L, RST_OUT_H, RST_OUT_L, done}
-                    cw <= 15'b__0_____0_____0_____0________0_______00________0_____00_______0_________0________0__________0_________0;                    
+                    cw <= 15'b__0_____0_____0_____1________0_______00________0_____00_______0_________0________0__________0_________0;                    
                     end                    
                     end
                S7:
@@ -201,6 +212,7 @@ module Full_Calculator_CU(
         assign {En_F, En_X, En_Y, Go_Calc, Go_Div, Op_Calc, Sel_H, Sel_L, En_Out_H, En_Out_L, RST_OUT_H, RST_OUT_L, done} = cw;
         assign Calc_Mux_Sel = Calc_Mux_Sel_internal;
         assign Mul_Mux_Sel = Mul_Mux_Sel_internal;
+        assign cs = CS;
                 
 
                 
